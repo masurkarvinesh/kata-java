5fd13b4fe1e067223171c9f91f228d26d70b1d74
Vinesh Masurkar <vineshmasurkr@yahoo.com>
2019-12-07 16:57:36 -0500

    Adds strangereons project.

---
+++ strangereons/src/test/java/strangereons/AcceptanceTest.java
@@ -0,0 +1,14 @@
+package strangereons;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class AcceptanceTest {
+    @Test
+    public void canaryTest() throws Exception {
+        assertTrue(true);
+//        UI ui = new UI();
+//        assertEquals("Leeted: S3cr3t", ui.leetMessage("Secret"));
+    }
+}
